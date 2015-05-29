026456b5cf2154269b3b33602f03cf4551646c4b
Gabriela Montoya <gmontoya@frennes>
2015-05-29 14:31:48 +0200

    reorganization of scripts

---
+++ scripts/restore.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+l="watDiv watDiv100 diseasome swdf geoCoordinates linkedMDB"
+for f in $l; do
+        files="$fedrahome/data/${f}Setup/endpointsDescription $fedrahome/data/${f}Setup/federation.ttl $fedrahome/data/${f}Setup/dawIndex.ttl $fedrahome/data/${f}Setup/fedraFiles/endpoints"
+        for file in $files; do
+            cp ${file}.copy ${file}
+        done
+done
