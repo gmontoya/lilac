523fe4ed33995bffcfbf86e5561e579393fac4a9
Gabriela Montoya <gmontoya@grenoble.grid5000.fr>
2015-09-03 18:48:54 +0200

    minor changes

---
+++ scripts/produceAnswersVirtuoso.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+file=$1
+l=`cat ${file}`
+l="$l"
+queriesFile=$2
+host=$3
+localPort=$4
+answersFolder=$5
+
+queryFile=`mktemp`
+tmpFile=`mktemp`
+
+for qn in $l; do
+    if [ ! -f $answersFolder/query${qn} ]; then
+        (head -n "$qn" "$queriesFile" | tail -n 1) > $queryFile
+        cd ${fusekiPath}
+        ./s-query --service http://${host}:${localPort}/sparql --file=${queryFile} --output=json > "$answersFolder/query${qn}"
+        cd ${fedrahome}/scripts
+        python formatJSONFile.py "$answersFolder/query${qn}" > "$tmpFile"
+        LANG=En_US sort "$tmpFile" > "$answersFolder/query${qn}"
+    fi
+done
+
+rm $tmpFile
+rm $queryFile
