b2263d606cbe28ad46bb72791f0c1cfaac3d6443
gmontoya <gmontoya@users.noreply.github.com>
2021-07-14 14:18:57 +0200

    Update uncompressData.sh

--- scripts/uncompressData.sh
+++ scripts/uncompressData.sh
@@ -4,7 +4,7 @@ federations="watDiv diseasome swdf linkedMDB geoCoordinates watDiv100"
 cd ${hdtJavaPath}
 for i in `seq 3030 3039`; do
     for f in $federations; do
-        ./hdt2rdf.sh ${lilachome}/data/${f}Setup/endpoint${i}.hdt ${lilachome}/data/${f}Setup/endpoint${i}.nt
+        ./bin/hdt2rdf.sh ${lilachome}/data/${f}Setup/endpoint${i}.hdt ${lilachome}/data/${f}Setup/endpoint${i}.nt
         rm ${lilachome}/data/${f}Setup/endpoint${i}.hdt
     done
 done
