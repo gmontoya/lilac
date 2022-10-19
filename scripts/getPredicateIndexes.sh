9e8a52d7dae651e1b0ea26afbdf0207274e93ad9
gmontoya <gabrielamontoyac@gmail.com>
2022-10-19 17:15:39 +0200

    adding predicate index files

---
+++ scripts/getPredicateIndexes.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+federations="diseasome swdf watDiv linkedMDB geoCoordinates watDiv100"
+
+cd ${lilachome}/code
+
+for f in $federations; do
+    java -cp .:${jenaPath}/lib/* generatePredicateIndex ${lilachome}/data/${f}Setup/fedraFiles/fragments ${lilachome}/data/${f}Setup/fedraFiles/predicateIndex
+done
+
